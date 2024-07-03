9291718abf1c8cc09134e50ad10e1e39ed18e617
bharath653 <bharathn653@gmail.com>
2024-07-03 10:27:43 +0530

    conflict resolve

--- src/pages/Admin/UserAdministration.tsx
+++ src/pages/Admin/UserAdministration.tsx
@@ -9,7 +9,7 @@ const UserAdministration  : FC=()=>{
         {name:"Email", datan:"Email"},
         {name:"Store", datan:"Store"},
         {name:"Type", datan:"Type"},
-        {name:"Actions", datan:"Actions",Call:()=><div><CustomTextButton children={"Remove"}/><CustomTextButton/></div>}
+        {name:"Actions", datan:"Actions",Call:()=><div><CustomTextButton children={"Remove"}/><CustomTextButton children={"jfgf"}/></div>}
       ]
      const Activedata = [
         {FirstName:'Jenny',LastName:"Morales",Email:"jennym@gmail.com",Store:"Store 1",Type:"Admin"},
