ec16deb2104837e0f38ed1280f7b0830649a40c2
bharath653 <bharathn653@gmail.com>
2024-07-05 14:57:56 +0530

    updated custom button

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 import { createTheme, ThemeProvider } from '@mui/material';
 import React from 'react' 
 import { BrowserRouter, Route } from 'react-router-dom';
-import ThemeRoutes from 'Router';
+import ThemeRoutes from './Router/index';
 type AppProps = {
     message: string;
 };
--- src/Router/index.tsx
+++ src/Router/index.tsx
@@ -1,62 +1,64 @@
-// import React, { useEffect, useState } from "react";
-// import { useRoutes, Navigate } from "react-router-dom";
-// import PrivateRoute from "./PrivateRoute";
-// import PublicRoute from "./PublicRoute";
-
-// const ThemeRoutes: React.FC = () => {
-//   const [isAuthenticated, setIsAuthenticated] = useState(false);
-//   useEffect(() => {
-//     const isLoggedIn = localStorage.getItem("isLoggedIn") === 'true';
-//     setIsAuthenticated(isLoggedIn);
-// }, []);
-
-//   let routes = isAuthenticated? PrivateRoute : PublicRoute;
-
-//   console.log("routes", routes);
-
-//   return useRoutes([
-//    ...routes,
-//     { path: "*", element: <Navigate to="/" replace /> },
-//   ]);
-// };
-
-// export default ThemeRoutes;
-import React, { useEffect, useState, createContext, useContext, FC } from "react";
+import React, { useEffect, useState } from "react";
 import { useRoutes, Navigate } from "react-router-dom";
 import PrivateRoute from "./PrivateRoute";
 import PublicRoute from "./PublicRoute";
 
-interface AuthContextProps {
-  isAuthenticated: boolean;
-  signIn: () => void;
-}
-
-const AuthContext = createContext<AuthContextProps | undefined>(undefined);
-
 const ThemeRoutes: React.FC = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
-
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const tk = localStorage.getItem("isLoggedIn");
   useEffect(() => {
     const isLoggedIn = localStorage.getItem("isLoggedIn") === 'true';
     setIsAuthenticated(isLoggedIn);
-  }, []);
+}, [tk]);
 
-  const signIn = () => {
-    localStorage.setItem('isLoggedIn', 'true');
-    setIsAuthenticated(true);
-  };
+  let routes = tk ? PrivateRoute : PublicRoute;
 
-  const routes = isAuthenticated ? PrivateRoute : PublicRoute;
+  console.log("routes", routes);
 
-  return (
-    <AuthContext.Provider value={{ isAuthenticated, signIn }}>
-      {useRoutes([
-        ...routes,
-        { path: "*", element: <Navigate to="/" replace /> },
-      ])}
-    </AuthContext.Provider>
-  );
+  return useRoutes([
+   ...routes,
+    { path: "*", element: <Navigate to="/" replace /> },
+  ]);
 };
 
-export default ThemeRoutes;
-export { AuthContext };
+ export default ThemeRoutes;
+
+// import React, { useEffect, useState, createContext, useContext, FC } from "react";
+// import { useRoutes, Navigate } from "react-router-dom";
+// import PrivateRoute from "./PrivateRoute";
+// import PublicRoute from "./PublicRoute";
+
+// interface AuthContextProps {
+//   isAuthenticated: boolean;
+//   signIn: () => void;
+// }
+
+// const AuthContext = createContext<AuthContextProps | undefined>(undefined);
+
+// const ThemeRoutes: React.FC = () => {
+//   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
+//   useEffect(() => {
+//     const isLoggedIn = localStorage.getItem("isLoggedIn") === 'true';
+//     setIsAuthenticated(isLoggedIn);
+//   }, []);
+
+//   const signIn = () => {
+//     localStorage.setItem('isLoggedIn', 'true');
+//     setIsAuthenticated(true);
+//   };
+
+//   const routes = isAuthenticated ? PrivateRoute : PublicRoute;
+
+//   return (
+//     <AuthContext.Provider value={{ isAuthenticated, signIn }}>
+//       {useRoutes([
+//         ...routes,
+//         { path: "*", element: <Navigate to="/" replace /> },
+//       ])}
+//     </AuthContext.Provider>
+//   );
+// };
+
+// export default ThemeRoutes;
+// export { AuthContext };
