bf20cafecc6c4104d3c6ca64f02f0cad5c5e9b63
alvitacastelino <98alvita@gmail.com>
2024-07-08 13:38:33 +0530

    update text button component

--- src/components/CustomOutLinedButton.tsx
+++ src/components/CustomOutLinedButton.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Button from '@mui/material/Button';
 import { styled } from '@mui/material/styles';
+import { MDBBtn } from 'mdb-react-ui-kit'; 
 
 interface CustomButtonProps {
   
@@ -28,10 +29,23 @@ const StyledButton = styled(Button)<{ customcolor?: string; customfontsize?: str
 
 const CustomOutLinedButton: React.FC<CustomButtonProps> = ({  fontSize, color, onClick,width, children }) => {
     return (
-        <StyledButton variant='outlined'  customcolor={color} width={width} customfontsize={fontSize} onClick={onClick}>
-            {children}
-        </StyledButton>
-    );
+        <MDBBtn
+          outline
+          onClick={onClick}
+          style={{
+            padding: '8.5px 15px',
+            height: '45px',
+            fontWeight: 600,
+            width: width || '100%',
+            fontSize: fontSize || '13px',
+            color: color || '#1266F1',
+            textTransform: 'none',
+            border: '2px solid',
+          }}
+        >
+          {children}
+        </MDBBtn>
+      );
 };
 
 export default CustomOutLinedButton;
--- src/components/CustomRadioGroup.tsx
+++ src/components/CustomRadioGroup.tsx
@@ -1,4 +1,51 @@
+// import React from 'react';
+// import {
+//   Radio,
+//   RadioGroup,
+//   FormControlLabel,
+//   Box
+// } from '@mui/material';
+// import { styled } from '@mui/system';
+
+// type Option = {
+//   label: string;
+//   value: string;
+// };
+
+// type Props = {
+//   options: Option[];
+//   onValueChange: (value: string) => void;
+//   selectedValue: string;
+//   row?: boolean;
+// };
+
+// const CustomRadioGroup: React.FC<Props> = ({ options, onValueChange, selectedValue, row }) => {
+//   const handleRadioChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+//     onValueChange(event.target.value);
+//   };
+
+//   return (
+//     <RadioGroup row={row} value={selectedValue} onChange={handleRadioChange}>
+//       {options.map((option) => (
+//         <FormControlLabel
+//           key={option.value}
+//           value={option.value}
+//           control={<Radio />}
+//           label={option.label}
+//           sx={{color:'#424242'}}
+          
+//         />
+//       ))}
+//     </RadioGroup>
+//   );
+// };
+
+// export default CustomRadioGroup;
+
+
+
 import React from 'react';
+import { MDBRadio } from 'mdb-react-ui-kit';
 import {
   Radio,
   RadioGroup,
@@ -27,14 +74,17 @@ const CustomRadioGroup: React.FC<Props> = ({ options, onValueChange, selectedVal
   return (
     <RadioGroup row={row} value={selectedValue} onChange={handleRadioChange}>
       {options.map((option) => (
-        <FormControlLabel
-          key={option.value}
-          value={option.value}
-          control={<Radio />}
-          label={option.label}
-          sx={{color:'#424242'}}
-          
-        />
+        <div key={option.value} className={`form-check ${row ? 'form-check-inline' : ''}`}>
+          <MDBRadio
+            id={option.value}
+            checked={option.value === selectedValue}
+            onChange={handleRadioChange}
+            value={option.value}
+          />
+          <label className="form-check-label" htmlFor={option.value}>
+            {option.label}
+          </label>
+        </div>
       ))}
     </RadioGroup>
   );
