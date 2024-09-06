7cb42e3ff226633e25cab1829c8cb23618d78a0b
bharath653 <143155922+sdsadmins@users.noreply.github.com>
2024-09-06 15:00:51 +0530

    updated

---
+++ src/pages/Admin/CreateNewUser.tsx
@@ -0,0 +1,76 @@
+import { Typography } from "@mui/material";
+import CardComponent from "components/CardComponent";
+import CustomButton from "components/CustomButton";
+import CustomInputComponent from "components/CustomInputComponent";
+import CustomOutLinedButton from "components/CustomOutLinedButton";
+import CustomSelectComponent from "components/CustomSelect";
+import CustomTextButton from "components/CustomTextButton";
+import { FC, useState } from "react";
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import { useNavigate } from "react-router-dom";
+import '../../css/CREATE_NEW_CUSTOMER_ACCOUNT.css';
+
+const CreateNewUser:FC=()=>{
+    const navigate = useNavigate();
+    const [role,setRole]=useState('');
+
+    const selects=(data:any)=>{
+        setRole(data);
+    }
+
+//--------------------------Edit UserForm----------------------------------------//
+      const RoleOptions = [{label:'Admin',value:"Admin"},{label:'User',value:"User"}]
+      const haedingBorder = {
+        borderBottom: '1px solid rgba(224, 224, 224, 1)',
+        fontFamily: `"Open Sans", sans-serif`,
+      };
+    
+      const headingStyle = {
+        fontSize: '16px',
+        fontWeight: 600,
+        color: '#1266F1',
+        lineHeight: '19.2px',
+        fontFamily: `"Open Sans", sans-serif`,
+        margin:'0px 0px 11px 7px'
+      };
+  const userForm = (
+    <div  style={{ display: 'flex', width: '100%', padding:'13px 20px 30px 20px', flexDirection: 'column', justifyContent: 'center' }}>
+          <div style={haedingBorder}>
+            <p style={headingStyle}>CREATE NEW USER</p>
+          </div>
+          <div style={{display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center'}}>
+          <div className="createNew-inputDiv" style={{margin:'20px 0px 18px 0px'}}>
+            <CustomInputComponent label="First Name" name="FirstName" />
+            <CustomInputComponent label="Last Name" name="LastName" />
+            <CustomInputComponent label="Email" name="Email" />
+          </div>
+          <div className="createNew-inputDiv" style={{marginBottom:'21px'}}>
+            <CustomInputComponent label="State" name="State" />
+            <CustomInputComponent label="City" name="City" />
+            <CustomInputComponent label="Zip Code" name="Zip Code" />
+          </div> 
+          </div>
+        </div>
+  )
+  //------------------------------------------------------------------------//
+  
+return(
+  <>
+   <div>
+        <CardComponent width="100%" children={userForm} backgroundColor={"#fafcfc"} />
+   </div> 
+   <div>
+     <div style={{display:'flex',justifyContent:'flex-start'}}>
+            <div><AddCircleIcon sx={{color:'#1266F1',backgroundColor:'white',borderRadius:'100%'}}/><span style={{color:'#1266F1',fontWeight:600,fontSize:'13px',lineHeight:'28px'}}>Add Another</span></div>
+            </div>
+            <div style={{display:'flex',flexDirection:'row',justifyContent:'flex-end'}}>
+                <div style={{display:'flex',justifyContent:'space-between',gap:'4px'}}>
+                    <CustomTextButton children={"Cancel"} width="60px" onClick={()=>navigate('/UserAdministration')}/>
+                    <CustomButton children={"Save"} width="79px"/>
+                </div>
+            </div>
+   </div>
+   </>
+)
+}
+export default CreateNewUser;
---
+++ src/pages/Admin/UserAdministration.tsx
@@ -0,0 +1,199 @@
+import { Typography } from "@mui/material";
+import CustomButton from "components/CustomButton";
+import CustomModal from "components/CustomModal";
+import CustomTable from "components/CustomTable";
+import CustomTextButton from "components/CustomTextButton";
+import TableTextButton from "components/TableTextButton";
+import { FC, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { AppState } from "./../../redux/rootReducer";
+
+const UserAdministration  : FC=()=>{
+    const [isModalOpen, setModalOpen] = useState(false);
+    const [modalStep, setModalStep] = useState(1);
+    const navigate = useNavigate();
+   //-----------------Redux Calling-------------------------//
+   const isOverflow = useSelector((state: AppState) => state.overflow.isOverflow);
+   console.log("isOverflow", isOverflow);
+   //-------------------------------------------------------//
+
+    const handleRestore = () => {
+      setModalStep(2);
+      setModalOpen(true);
+    };
+  
+
+    //----------------------------Active Table data ----------------------------------//
+      const ActiveColumns =[
+        {name:"First Name", datan:"FirstName"},
+        {name:"Last Name", datan:"LastName"},
+        {name:"Email", datan:"Email"},
+        {name:"Store", datan:"Store"},
+        {name:"Type", datan:"Type"},
+        {name:"Actions", datan:"Actions",
+          Call: (row: any) => (
+            <div style={{ display: "flex", flexDirection: "row" }}>
+              <TableTextButton onClick={removeFunction} width="59px">
+                Remove
+              </TableTextButton>
+              <TableTextButton
+                
+                onClick={() => {
+                  navigate("/EditUser", { state: row });
+                }}
+                width="37px" >
+                Edit
+              </TableTextButton>
+            </div>
+          ),
+        }
+      ]
+     const Activedata = [
+        {FirstName:'Jenny',LastName:"Morales",Email:"jennym@gmail.com",Store:"Store 1",Type:"Admin"},
+        {FirstName:'Tom',LastName:"Johnson",Email:"tomj@gmail.com",Store:"Store 2",Type:"User"},
+        {FirstName:'Jamie',LastName:"King",Email:"jamieking@gmail.com",Store:"Store 3",Type:"Admin"}
+     ]
+
+     const removeFunction=()=>{
+        setModalOpen(true);
+     }
+  //------------------------------------------------------------------------------------//
+
+
+  //-------------------------------------------InACtive Table Data------------------------------//
+
+  const InActiveColumns =[
+    {name:"First Name", datan:"FirstName"},
+    {name:"Last Name", datan:"LastName"},
+    {name:"Email", datan:"Email"},
+    {name:"Store", datan:"Store"},
+    {name:"Type", datan:"Type"},
+    {name:"Actions", datan:"Actions",
+      Call: (row: any) => (
+        <div style={{ display: "flex", flexDirection: "row" }}>
+          <TableTextButton onClick={handleRestore} width="57px">
+            Restore
+          </TableTextButton>
+          {/* <TableTextButton
+            
+            onClick={() => {
+              navigate("/EditUser", { state: row });
+            }}
+          >
+            Edit
+          </TableTextButton> */}
+        </div>
+      ),
+    }
+  ]
+ const InActivedata = [
+    {FirstName:'Jenny',LastName:"Morales",Email:"jennym@gmail.com",Store:"Store 1",Type:"Admin"},
+    {FirstName:'Tom',LastName:"Johnson",Email:"tomj@gmail.com",Store:"Store 2",Type:"User"},
+    {FirstName:'Jamie',LastName:"King",Email:"jamieking@gmail.com",Store:"Store 3",Type:"Admin"}
+ ]
+
+
+ //---------------------------------------------------------------------------------------------//
+
+
+
+     //-----------------------------------Remove Modal----------------------//
+      const handleCloseModal = () => {
+        setModalOpen(false);
+      };
+    
+      const RemoveModaltitle = (
+        <p style={{ fontSize: '20px', fontWeight: 600, color: '#424242', textAlign: 'center', lineHeight: '24px' }}>Remove User</p>
+      )
+      const RemoveModalbody = (
+        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+          <p style={{color:'#424242'}}>Are you sure you want to remove user?</p>
+        </div>
+      )
+    
+      const RemoveModalfooter = (
+        <div style={{ display: 'flex', flexDirection: 'row' }}>
+            <CustomTextButton children={"Cancel"} width="100px" onClick={handleCloseModal}/>
+            <CustomButton children={"Yes"} width="74px" />
+        </div>
+      )
+     //----------------------------------------------------------------------//
+
+
+//-----------------------------------Restore Modal----------------------//
+     const RestoreModalTitle = (
+      <p style={{ fontSize: '20px', fontWeight: 600, color: '#424242', textAlign: 'center', lineHeight: '24px' }}>Restore Store</p>
+    )
+
+    
+    const RestoreModalBody = (
+      <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+        <p>Are you sure you want to restore store?</p>
+      </div>
+    );
+
+    const RestoreModalFooter = (
+      <div style={{ display: 'flex', flexDirection: 'row', gap: '5px' }}>
+        <CustomTextButton onClick={handleCloseModal}>Cancel</CustomTextButton>
+        <CustomButton width="74px">Yes</CustomButton>
+      </div>
+    );
+  //----------------------------------------------------------------------//
+
+
+  const title = modalStep === 1 ? RemoveModaltitle: RestoreModalTitle;
+  const body = modalStep === 1 ? RemoveModalbody : RestoreModalBody;
+  const footer = modalStep === 1 ? RemoveModalfooter : RestoreModalFooter;
+
+
+    return(
+        <div>
+        <CustomModal
+          open={isModalOpen}
+          onClose={handleCloseModal}
+          title={() => title}
+          body={() => body}
+          footer={() => footer}
+          animation={true}
+          size="medium"
+          centered={true}
+        />
+            <div>
+            <div>
+            <p style={{color:'#1266F1',marginBottom:'20px',fontWeight:600,fontSize:'16px',lineHeight:'19.2px'}}>Active</p>
+            <CustomTable columns={ActiveColumns} data={Activedata}/>
+            </div>
+            <div style={{marginTop:'50px'}}>
+            <p style={{color:'#1266F1',marginBottom:'20px',fontWeight:600,fontSize:'16px',lineHeight:'19.2px'}}>Inactive</p>
+            <CustomTable columns={InActiveColumns} data={InActivedata}/>
+            </div>
+            {/* <div style={{display:'flex',flexDirection:'row',justifyContent:'flex-end',gap:'2px'}}> */}
+      
+
+              {isOverflow ?
+                <div style={{ width:'100%',display: 'flex', flexDirection: 'row',justifyContent:'flex-end',marginTop:'15px'}}>
+         <div>
+                <CustomTextButton children={"Back"} width="61px"/>
+                <CustomButton children={"Create New User"} width="148px" onClick={()=>navigate('/CreateNewUser')}/>
+              </div>
+      </div>
+      :
+<div style={{position:'fixed',bottom:'10px',width:'100%'}}>
+  <div style={{ width:'100%',display: 'flex', flexDirection: 'row',justifyContent:'flex-end',padding:'0px 40px'}}>
+  <div>
+                <CustomTextButton children={"Back"} width="61px"/>
+                <CustomButton children={"Create New User"} width="148px" onClick={()=>navigate('/CreateNewUser')}/>
+              </div>
+      </div>
+
+</div>
+}
+
+
+            {/* </div> */}
+            </div>
+            </div>
+    )
+}
+export default UserAdministration;
